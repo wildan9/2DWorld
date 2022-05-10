7b0326f959b3e6891023306c531796226586c526
route-x <optimus102001@gmail.com>
2022-05-10 09:33:00 +0700

    Delete GetterSetter.h and move the code to Camera2D.h

--- walking/Camera2D.h
+++ walking/Camera2D.h
@@ -3,7 +3,8 @@
 #pragma once
 
 #include "Header.h"
-#include "GetterSetter.h"
+
+#define GETTERSETTER(type, method, name) inline type Get##method() const { return name; } inline void Set##method(type value) { name = value; }
 
 namespace raylib 
 {
--- walking/GetterSetter.h
+++
@@ -1,3 +0,0 @@
-/* by Wildan R. */
-
-#define GETTERSETTER(type, method, name) inline type Get##method() const { return name; } inline void Set##method(type value) { name = value; }
